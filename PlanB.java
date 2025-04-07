d95d020ab69caae452bb88c117ba18e3fdbf2162
Benjamin Gallarreta <benjamingallarreta@gmail.com>
2025-04-07 19:30:55 -0300

    update strategy

--- PlanB.java
+++ PlanB.java
@@ -1,14 +1,31 @@
+// Plan B - Aprobó los finales de las correlativas 
 public class PlanB implements StrategyPlanEstudio {
-    
-    @Override
-    public class verificarCondicion (Materia materia, Alumno alumno) {
 
-        for(materia.correlativas) {
-            if (materia.correlativas(i).estado == aprobado) {
-                
+    @Override
+    public boolean verificarCondicion (MateriaCursada materia, Alumno alumno) {
+
+        //acceder a las materias correlativas de la materia pasada como parametro
+        //recorrer la lista de las materias correlativas
+        //verificar si esas materias correlativas tiene como estado "aprobado"
+        //inscribir alumno en caso de que esto suceda
+        
+        for (Materia correlativa : materia.correlativas) {
+            boolean aprobada = false;
+    
+            for (MateriaCursada cursada : materiasCursadas) {
+                if (cursada.getMateria().equals(correlativa) &&
+                    cursada.getEstado() == MateriaCursada.Estado.aprobado) {
+                    aprobada = true;
+                    break;
+                }
+            }
+    
+            if (!aprobada) {
+                return false; // una no aprobada, salimos
             }
         }
-
+    
+        return true; // todas aprobadas
     }
 
 }
